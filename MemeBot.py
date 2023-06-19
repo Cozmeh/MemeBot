0b8726ff7759308d5dc5d305b14056c570919240
Cozmeh <hemsainath15@gmail.com>
2023-06-19 21:34:06 +0530

    Bot starts

--- MemeBot.py
+++ MemeBot.py
@@ -1,1 +1,10 @@
 import discord
+import env
+
+Client = discord.Client(intents=discord.Intents.default())
+
+@Client.event #on ready (Bot start)
+async def on_ready():
+    print("Bot is ready!")
+
+Client.run(env.TOKEN) 
