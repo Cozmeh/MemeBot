985450ece230b04078ed1785cd6490b84e7ebf48
Hem <117145297+Cozmeh@users.noreply.github.com>
2023-06-24 12:15:44 +0530

    Create env.py

---
+++ env.py
@@ -0,0 +1,11 @@
+# paste your discord bot token 
+TOKEN = "Your discord bot token"
+
+# default reply message by the bot 
+DEFAULT = "Just a second..."
+
+# error message by the bot 
+ERROR = "Could not find any meme :/"
+
+# scraping source 
+SOURCE = "https://www.memedroid.com/memes/tag/"
